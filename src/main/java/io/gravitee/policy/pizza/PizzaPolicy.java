e07ef2d8e11d9d21a5c1949575fe38872105a7ad
Yann Tavernier <yann.tavernier@graviteesource.com>
2023-06-19 07:43:30 +0200

    feat: init policy - set minimal maven dependencies - set assembly folder - set plugin.properties - add an image

---
+++ src/main/java/io/gravitee/policy/pizza/PizzaPolicy.java
@@ -0,0 +1,11 @@
+package io.gravitee.policy.pizza;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class PizzaPolicy {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
